4d8d8c75e71c94dd6f19719ed9fea3fac77ba7b9
ronadomessisilva-create <ronadomessisilva@gmail.com>
2026-01-15 02:22:28 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 This contains everything you need to run your app locally.
 
-View your app in AI Studio: https://ai.studio/apps/drive/1hE9Hc1WjkF2-dir641LWoZfFOyVPPb0K
+View your app in AI Studio: https://ai.studio/apps/temp/1
 
 ## Run Locally
 
--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
 
-import '@angular/compiler'; // JIT compilation is required
+ // JIT compilation is required
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './src/app.component';
